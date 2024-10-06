95f3456023d0a13922d13c2c202b349c3ecba4e8
unclebiglu <codeerror0x01@gmail.com>
2024-10-06 17:52:51 +0800

    adc runs succesfully

--- main/adc_fft.c
+++ main/adc_fft.c
@@ -1,6 +1,121 @@
 #include <stdio.h>
+#include <string.h>
 
-void app_main(void)
-{
+#include "esp_adc/adc_continuous.h"
+#include "esp_dsp.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
 
+#define EXAMPLE_ADC_UNIT ADC_UNIT_1
+#define _EXAMPLE_ADC_UNIT_STR(unit) #unit
+#define EXAMPLE_ADC_UNIT_STR(unit) _EXAMPLE_ADC_UNIT_STR(unit)
+#define EXAMPLE_ADC_CONV_MODE ADC_CONV_SINGLE_UNIT_1
+#define EXAMPLE_ADC_ATTEN ADC_ATTEN_DB_0
+#define EXAMPLE_ADC_BIT_WIDTH SOC_ADC_DIGI_MAX_BITWIDTH
+
+#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
+#define EXAMPLE_ADC_OUTPUT_TYPE ADC_DIGI_OUTPUT_FORMAT_TYPE1
+#define EXAMPLE_ADC_GET_CHANNEL(p_data) ((p_data)->type1.channel)
+#define EXAMPLE_ADC_GET_DATA(p_data) ((p_data)->type1.data)
+#else
+#define EXAMPLE_ADC_OUTPUT_TYPE ADC_DIGI_OUTPUT_FORMAT_TYPE2
+#define EXAMPLE_ADC_GET_CHANNEL(p_data) ((p_data)->type2.channel)
+#define EXAMPLE_ADC_GET_DATA(p_data) ((p_data)->type2.data)
+#endif
+
+/* Size of ADC conversion frame
+ * Number of ADC result per frame equals to
+ * ADC_CONV_FRAME_SZ/SOC_ADC_DIGI_RESULT_BYTES
+ */
+#define ADC_CONV_FRAME_SZ 256
+#define ADC_SAMPLE_RATE 20e3
+
+static const char *TAG = "main";
+
+// As we're using ESP32S3...
+// Only one channel is required
+// Should map to GPIO 3
+static adc_channel_t channel[1] = {ADC_CHANNEL_2};
+
+static TaskHandle_t s_task_handle;
+
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle,
+                                     const adc_continuous_evt_data_t *edata,
+                                     void *user_data) {
+    BaseType_t mustYield = pdFALSE;
+    // Notify that ADC continuous driver has done enough number of conversions
+    vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
+
+    return (mustYield == pdTRUE);
+}
+
+static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num,
+                                adc_continuous_handle_t *out_handle) {
+    adc_continuous_handle_t handle = NULL;
+
+    adc_continuous_handle_cfg_t adc_config = {
+        .max_store_buf_size = 1024,
+        .conv_frame_size = ADC_CONV_FRAME_SZ,
+    };
+    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
+
+    adc_continuous_config_t dig_cfg = {
+        .sample_freq_hz = ADC_SAMPLE_RATE,
+        .conv_mode = EXAMPLE_ADC_CONV_MODE,
+        .format = EXAMPLE_ADC_OUTPUT_TYPE,
+    };
+
+    adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
+    dig_cfg.pattern_num = channel_num;
+    for (int i = 0; i < channel_num; i++) {
+        adc_pattern[i].atten = EXAMPLE_ADC_ATTEN;
+        adc_pattern[i].channel = channel[i] & 0x7;
+        adc_pattern[i].unit = EXAMPLE_ADC_UNIT;
+        adc_pattern[i].bit_width = EXAMPLE_ADC_BIT_WIDTH;
+
+        ESP_LOGI(TAG, "adc_pattern[%d].atten is :%" PRIx8, i,
+                 adc_pattern[i].atten);
+        ESP_LOGI(TAG, "adc_pattern[%d].channel is :%" PRIx8, i,
+                 adc_pattern[i].channel);
+        ESP_LOGI(TAG, "adc_pattern[%d].unit is :%" PRIx8, i,
+                 adc_pattern[i].unit);
+    }
+    dig_cfg.adc_pattern = adc_pattern;
+    ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
+
+    *out_handle = handle;
+}
+void app_main(void) {
+    esp_err_t ret;
+    uint32_t ret_num = 0;
+    uint8_t result[ADC_CONV_FRAME_SZ] = {0};
+    memset(result, 0xcc, ADC_CONV_FRAME_SZ);
+
+    s_task_handle = xTaskGetCurrentTaskHandle();
+
+    /* ADC initial */
+    adc_continuous_handle_t adc_handle = NULL;
+    continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t),
+                        &adc_handle);
+
+    /* Register conversion done callback */
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = s_conv_done_cb,
+    };
+    ESP_ERROR_CHECK(
+        adc_continuous_register_event_callbacks(adc_handle, &cbs, NULL));
+    ESP_ERROR_CHECK(adc_continuous_start(adc_handle));
+
+    while (1) {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        ret =
+            adc_continuous_read(adc_handle, result, ADC_CONV_FRAME_SZ, &ret_num, 0);
+        if (ret == ESP_OK) {
+            ESP_LOGI(TAG, "%d results per frame",
+                     ADC_CONV_FRAME_SZ / SOC_ADC_DIGI_RESULT_BYTES);
+        }
+    }
 }
