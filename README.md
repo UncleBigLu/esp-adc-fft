0e7f485a593c4d69b139146a2ba40d3fac8dc52f
unclebiglu <codeerror0x01@gmail.com>
2024-10-07 20:50:40 +0800

    plot fft graph periodically

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# ADC-FFT
+# ESP-ADC-FFT
 
 Sample input signal with **ESP32S3** on board ADC and perform FFT with esp-dsp library. FFT result will be plotted periodically.
+
+Currently using **GPIO 3** as signal input. Sampling frequency at **20 KHz**
--- main/adc_fft.c
+++ main/adc_fft.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -26,12 +27,24 @@
 #define EXAMPLE_ADC_GET_DATA(p_data) ((p_data)->type2.data)
 #endif
 
+#define FFT_POINTS 2048
 /* Size of ADC conversion frame
  * Number of ADC result per frame equals to
  * ADC_CONV_FRAME_SZ/SOC_ADC_DIGI_RESULT_BYTES
  */
 #define ADC_CONV_FRAME_SZ 256
 #define ADC_SAMPLE_RATE 20e3
+#define ADC_MAX_STORE_BUF_SZ 1024
+
+#define ADC_READ_CHECK(x)                                   \
+    do {                                                    \
+        esp_err_t err_rc_ = (x);                            \
+        if (err_rc_ == ESP_ERR_INVALID_STATE) {             \
+            ESP_LOGW(TAG, "ADC internal buf full.");        \
+        } else if (err_rc_ == ESP_ERR_TIMEOUT) {            \
+            ESP_LOGW(TAG, "ADC reports NO data available"); \
+        }                                                   \
+    } while (0)
 
 static const char *TAG = "main";
 
@@ -42,25 +55,45 @@ static adc_channel_t channel[1] = {ADC_CHANNEL_2};
 
 static TaskHandle_t s_task_handle;
 
+// Hann window for FFT
+static float wind[FFT_POINTS] __attribute__((aligned(16)));
+static uint8_t adc_buf[FFT_POINTS * SOC_ADC_DIGI_RESULT_BYTES] = {0};
+// fft_buf[2 * i + 0] contains real, fft_buf[2 * i + 1] contains imaginary
+static float fft_buf[2 * FFT_POINTS] __attribute__((aligned(16)));
+static float spectrum[FFT_POINTS / 2] __attribute__((aligned(16)));
+
 static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle,
                                      const adc_continuous_evt_data_t *edata,
                                      void *user_data) {
     BaseType_t mustYield = pdFALSE;
-    // Notify that ADC continuous driver has done enough number of conversions
+    // Notify that ADC continuous driver has done enough number of
+    // conversions
     vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
-
     return (mustYield == pdTRUE);
 }
 
 static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num,
                                 adc_continuous_handle_t *out_handle) {
+    esp_err_t ret;
+
     adc_continuous_handle_t handle = NULL;
 
     adc_continuous_handle_cfg_t adc_config = {
-        .max_store_buf_size = 1024,
+        .max_store_buf_size = ADC_MAX_STORE_BUF_SZ,
         .conv_frame_size = ADC_CONV_FRAME_SZ,
     };
-    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
+    ret = adc_continuous_new_handle(&adc_config, &handle);
+    switch (ret) {
+        case ESP_ERR_NO_MEM:
+            ESP_LOGE(TAG, "out of memory");
+            break;
+        case ESP_ERR_NOT_FOUND:
+            ESP_LOGE(TAG, "not found");
+            break;
+        case ESP_ERR_INVALID_ARG:
+            ESP_LOGE(TAG, "invalid arg");
+            break;
+    }
 
     adc_continuous_config_t dig_cfg = {
         .sample_freq_hz = ADC_SAMPLE_RATE,
@@ -88,13 +121,22 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num,
 
     *out_handle = handle;
 }
+
+static void fft_init(void) {
+    ESP_ERROR_CHECK(dsps_fft2r_init_fc32(NULL, CONFIG_DSP_MAX_FFT_SIZE));
+    ESP_ERROR_CHECK(dsps_fft4r_init_fc32(NULL, CONFIG_DSP_MAX_FFT_SIZE));
+    // Generate hann window
+    dsps_wind_hann_f32(wind, FFT_POINTS);
+}
+
 void app_main(void) {
-    esp_err_t ret;
-    uint32_t ret_num = 0;
-    uint8_t result[ADC_CONV_FRAME_SZ] = {0};
-    memset(result, 0xcc, ADC_CONV_FRAME_SZ);
+    size_t i;
+    uint32_t ret_num = 0, tot_read_num = 0;
+    memset(adc_buf, 0xcc, FFT_POINTS * SOC_ADC_DIGI_RESULT_BYTES);
 
     s_task_handle = xTaskGetCurrentTaskHandle();
+
+    fft_init();
 
     /* ADC initial */
     adc_continuous_handle_t adc_handle = NULL;
@@ -107,15 +149,58 @@ void app_main(void) {
     };
     ESP_ERROR_CHECK(
         adc_continuous_register_event_callbacks(adc_handle, &cbs, NULL));
+    /* Start ADC */
     ESP_ERROR_CHECK(adc_continuous_start(adc_handle));
 
     while (1) {
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        ret =
-            adc_continuous_read(adc_handle, result, ADC_CONV_FRAME_SZ, &ret_num, 0);
-        if (ret == ESP_OK) {
-            ESP_LOGI(TAG, "%d results per frame",
-                     ADC_CONV_FRAME_SZ / SOC_ADC_DIGI_RESULT_BYTES);
+        // Read ADC and fill fft buffer
+        do {
+            ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+            ADC_READ_CHECK(adc_continuous_read(
+                adc_handle, adc_buf + tot_read_num,
+                FFT_POINTS * SOC_ADC_DIGI_RESULT_BYTES - tot_read_num, &ret_num,
+                0));
+            tot_read_num += ret_num;
+        } while (tot_read_num < FFT_POINTS * SOC_ADC_DIGI_RESULT_BYTES);
+        tot_read_num = 0;
+
+        for (i = 0; i < FFT_POINTS * SOC_ADC_DIGI_RESULT_BYTES;
+             i += SOC_ADC_DIGI_RESULT_BYTES) {
+            adc_digi_output_data_t *p = (adc_digi_output_data_t *)&adc_buf[i];
+            uint32_t chan_num = EXAMPLE_ADC_GET_CHANNEL(p);
+            if (chan_num >= SOC_ADC_CHANNEL_NUM(EXAMPLE_ADC_UNIT)) {
+                ESP_LOGW(TAG, "invalid data");
+                continue;
+            }
+            // Real part of signal
+            fft_buf[2 * (i / SOC_ADC_DIGI_RESULT_BYTES) + 0] =
+                (float)EXAMPLE_ADC_GET_DATA(p) *
+                wind[i / SOC_ADC_DIGI_RESULT_BYTES];
+            // Only real part
+            fft_buf[2 * (i / SOC_ADC_DIGI_RESULT_BYTES) + 1] = 0;
+            // ESP_LOGI(TAG, "fft_buf[%u]: %f", 2 * (i /
+            // SOC_ADC_DIGI_RESULT_BYTES), fft_buf[2 * (i /
+            // SOC_ADC_DIGI_RESULT_BYTES)]);
         }
+        // FFT Radix-2
+        dsps_fft2r_fc32(fft_buf, FFT_POINTS);
+        dsps_bit_rev2r_fc32(fft_buf, FFT_POINTS);
+        dsps_cplx2real_fc32(fft_buf, FFT_POINTS);
+
+        for (i = 0; i < FFT_POINTS / 2; ++i) {
+            spectrum[i] = sqrtf(fft_buf[i * 2] * fft_buf[i * 2] +
+                                fft_buf[i * 2 + 1] * fft_buf[i * 2 + 1]) /
+                          FFT_POINTS;
+            // ESP_LOGI(TAG, "spectrum[%u]: %f", i, spectrum[i]);
+        }
+        // I don't want DC components so remove it
+        for (i = 0; i < 10; ++i) {
+            spectrum[i] = 0;
+        }
+        // data, len, width, height, min, max, char
+        dsps_view(spectrum, FFT_POINTS / 2, 128, 10, 0, 100, '|');
+
+        // Do not starve watch dog as print is slow
+        vTaskDelay(1);
     }
 }
